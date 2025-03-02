8a612a1ff6622af2996b0f41cbe54823a6a08046
Shivani Nandakumar <snand021@ucr.edu>
2025-03-02 15:36:13 -0800

    forward selection

--- main.py
+++ main.py
@@ -12,13 +12,27 @@ accuracy = leave_one_out_cross_validation(None, None, None)
 
 
 def feature_search_demo(data): #data is the data set youre intaking
-    dsize = data.shape() #get num of rows and column in dataset
+    dsize = data.shape #get num of rows and column in dataset
     dsize = dsize[1] #get number of columns (where attributes are)
+
+    current_set_of_features = [] #initialize empty list so we can keep track of items, Q: does it need to be set or list
     for i in range(1, dsize - 1): #first column is just label so it doesn't count
-        print(f"On the {str(i)}th level of the search tree")
-        for k in range(1, dsize-1):
-            print(f"On the {str(k)}th level of the search tree")
+        print(f"On the {i}th level of the search tree")
+        feature_to_add_at_this_level = None #should this be a set or is it jsut one val Q
+        best_so_far_accuracy = 0
+        for k in range(1, dsize-1): #these nested loops helps us traverse through the search space  
+            if k not in current_set_of_features:  #dont add duplicate features, make sure each feature is added only once
+                print(f"--Consider adding the {k} feature")
+                accuracy = leave_one_out_cross_validation(data,current_set_of_features, k+1) #we are looking to remember highest num
+                #get max
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_add_at_this_level = k #k feature gave us this accuracy so we want to add it
+                    
+        current_set_of_features.append(feature_to_add_at_this_level) #add new feature to current set since we chose it
+        print(f"On level {i} i added feature {feature_to_add_at_this_level} to current set")
 
 
-
+if __name__ == "__main__":
+    data = np.loadtxt('CS170_Small_Data_000.txt')
     
