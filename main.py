525586a4a642f931f736d4f98b6a60915a940116
Shivani Nandakumar <snand021@ucr.edu>
2025-03-02 14:33:42 -0800

    initial code stub

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+def leave_one_out_cross_validation(data, current_set, feature_to_add):
+    accuracy = random.random()
+    return accuracy
+
+    
+      
+accuracy = leave_one_out_cross_validation(None, None, None)
+   
